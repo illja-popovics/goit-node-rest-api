7dc95a82d3f129dbccfb1edd559f8c5764ec9f8c
illja-popovics <143592959+illja-popovics@users.noreply.github.com>
2025-03-14 08:31:22 +0100

    fixes based on feedback

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,15 @@
 import express from 'express';
-import multer from "multer";
 
-import { register, login, logout, getCurrentUser } from '../controllers/authController.js';
+import { register, login, logout, getCurrentUser, updateAvatar } from '../controllers/authController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { updateAvatar } from "../controllers/authController.js";
-
+import upload from '../middlewares/uploadMiddleware.js'; 
 
 const router = express.Router();
-const upload = multer({ dest: "temp/" }); // Temporary storage
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', authMiddleware, logout);
 router.get('/current', authMiddleware, getCurrentUser);
-router.patch("/avatars", authMiddleware, upload.single("avatar"), updateAvatar);
+router.patch("/avatars", authMiddleware, upload.single("avatar"), updateAvatar); 
 
 export default router;
